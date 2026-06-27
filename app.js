4875bc5de4f212969c4b5eda35ffac305f53042f
BRINDHADEVI18 <64881981+BRINDHADEVI18@users.noreply.github.com>
2026-06-27 12:02:01 +0530

    Create app.js

---
+++ app.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const dotenv = require('dotenv');
+const authRoutes = require('./routes/authRoutes');
+const userRoutes = require('./routes/userRoutes');
+
+
+dotenv.config();
+
+const app = express();
+
+
+app.use(express.json());
+
+// Routes
+app.use('/api/auth', authRoutes);
+app.use('/api/users', userRoutes);
+
+// Default route
+app.get('/', (req, res) => {
+  res.json({ message: 'API is running!' });
+});
+
+// Error handling middleware
+app.use((err, req, res, next) => {
+  console.error(err.message);
+  res.status(500).json({ message: 'Server Error' });
+});
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
+
+module.exports = app;
