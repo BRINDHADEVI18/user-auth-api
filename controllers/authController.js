11789569c1983b9edceef45961584bf842cc6e0f
BRINDHADEVI18 <64881981+BRINDHADEVI18@users.noreply.github.com>
2026-08-10 11:29:45 +0530

    Update authController.js

--- controllers/authController.js
+++ controllers/authController.js
@@ -46,7 +46,8 @@ const register = async (req, res) => {
       user: {
         id: user.id,
         name: user.name,
-        email: user.email
+        email: user.email,
+        role: user.role
       }
     });
 
