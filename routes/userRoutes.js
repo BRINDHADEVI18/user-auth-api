6b72e2cca07c3850a33c6ba73cd67802003affd6
BRINDHADEVI18 <64881981+BRINDHADEVI18@users.noreply.github.com>
2026-08-13 12:54:00 +0530

    Update userRoutes.js

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -4,12 +4,14 @@ const protect = require('../middleware/protect');
 const {
   getProfile,
   getAllUsers,
+  getMe,
   updateUser,
   deleteUser
 } = require('../controllers/userController');
 
 router.get('/profile', protect, getProfile);
 router.get('/', protect, getAllUsers);
+router.get('/me',protect,getMe);
 router.put('/:id', protect, updateUser);
 router.delete('/:id', protect, deleteUser);
 
