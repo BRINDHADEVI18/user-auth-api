a67e3d4b85e903b99fb249bd6ce791fc6c856b8f
BRINDHADEVI18 <64881981+BRINDHADEVI18@users.noreply.github.com>
2026-08-13 10:09:39 +0530

    Update authRoutes.js

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -12,6 +12,10 @@ const loginLimiter = rateLimit({
 
 
 router.post('/register', register);
-router.post('/login', loginLimiter, login);
+// router.post('/login', loginLimiter, login);
+
+router.post(
+  '/login',
+  loginLimiter, [ body('email').isEmail().withMessage('Valid email required'), body('password').notEmpty().withMessage('Password is required'),], validateRequest,authController.login);
 
 module.exports = router;
