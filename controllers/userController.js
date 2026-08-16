8409c44d5ad9f862d31e67ce5a6a9285887f31cb
BRINDHADEVI18 <64881981+BRINDHADEVI18@users.noreply.github.com>
2026-08-16 22:39:55 +0530

    Update userController.js

--- controllers/userController.js
+++ controllers/userController.js
@@ -19,11 +19,11 @@ const getAllUsers = async (req, res) => {
   try {
     const users = await User.findAll();
 
-    res.status(200).json({
-      message: 'Users fetched successfully',
-      count: users.length,
+    res.status(200).json(
+     // message: 'Users fetched successfully',
+    //  count: users.length,
       users
-    });
+    );
   } catch (err) {
     res.status(500).json({ message: err.message });
   }
